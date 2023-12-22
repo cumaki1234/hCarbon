780a93ef2b25ffae240ea1d4dc632eee969b67c1
bryan robalino <eduardochuez_2003@outlook.com>
2023-12-22 13:44:49 -0500

    xd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,37 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import {registroUsuario} from "./pages/registroUsuario";
+import logo from './assets/images/descargar.jpg';
+import {faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faUser, faBell } from '@fortawesome/free-solid-svg-icons';
+
+
+import "./App.css"
 
 function App() {
   return (
     
-    <BrowserRouter>
-      <Routes>
-        <Route path="/task" element={<registroUsuario />} />
-    </Routes>
-    </BrowserRouter>,
+    <div className="navbar">
+      <div> <img src={logo} alt="Logo" className="logo" /></div>
+      <div className="company-name">Hamburguesas La Carbon</div>
+      <div className="menu-items">
+        <button className="menu-button">Menú</button>
+        <button className="menu-button">Reservaciones</button>
+        <button className="menu-button">Puntos</button>
+        <a href="tu-enlace-de-WhatsApp" className="whatsapp-button">
+        <FontAwesomeIcon icon={faWhatsapp} className="whatsapp-icon" />
+        </a>
+        <button className="menu-button">
+          <FontAwesomeIcon icon={faUser} className="user-icon" />
+     
+        </button>
+        <button className="menu-button">
+          <FontAwesomeIcon icon={faBell} className="bell-icon" />
+        
+        </button>
+      </div>
+      </div>
+   
 
     
   );
